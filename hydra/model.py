7585f316b389c2c822c01ec854d60f4308c362d7
Urs Koster <urs@cerebras.net>
2019-03-23 21:42:54 -0700

    added missing FC and layer norm components

--- hydra/model.py
+++ hydra/model.py
@@ -8,6 +8,25 @@ import ipdb
 
 data = DataLoader()
 
+
+def relu(x):
+    """ stax relu"""
+    return xp.maximum(x, 0.)
+
+def layernorm(inputs):
+    """
+    Layer normalization across features, which are assumed to be the first dim.
+    Behaves as an activation function, no learned parameters.
+
+    Arguments:
+        inputs: Activations of shape [features x other]
+    """
+    mean = xp.mean(inputs, axis=1, keepdims=True)
+    meaned = inputs - mean
+    variance = xp.mean(meaned**2, axis=1, keepdims=True)
+    outputs = meaned / xp.sqrt(variance + 0.00001)
+    return outputs
+
 def softmax_cross_entropy(activation, target):
     """
     softmax_cross_entropy.
@@ -39,7 +58,8 @@ def transformer_forward(params, seq_x, seq_s):
         seq_s: Position encodings (3xNT)
     """
     # unpack parameters
-    weights_k, weights_v, weights_q, weights_fc = params
+    (weights_k, weights_v, weights_q, weights_o, weights_gamma1, weights_beta1,
+        weights_fc1, weights_fc2, weights_gamma2, weights_beta2) = params
     dim_K, dim_C, = weights_k.shape
     dim_NT = seq_x.shape[0]
     dim_N = 256
@@ -48,6 +68,9 @@ def transformer_forward(params, seq_x, seq_s):
     # package up data seq
     seq_onehot = xp.eye(data.embed_dim)[:,seq_x]
     inputs = xp.array(xp.vstack((seq_onehot, seq_s)))  # CN
+
+    # TODO: Adding input weights to match dimensionality for skip
+    # inputs = xp.dot(weights_in, inputs)
 
     # first multiply the query against the keys, [KNT] = [KC] [CNT]
     act_K = xp.dot(weights_k, inputs).reshape((dim_K, dim_N, dim_T))
@@ -58,12 +81,29 @@ def transformer_forward(params, seq_x, seq_s):
     attention = xp.exp(-attention) / xp.sum(xp.exp(-attention), axis=1)
 
     # then compute the weighted values
-    inputs = xp.einsum('ijn,kni->knj', attention, act_V)  # [TTN][KNT]=[KNT]
-    inputs = inputs.reshape((dim_K, dim_N * dim_T))  # pack NT back for FC
+    attention = xp.einsum('ijn,kni->knj', attention, act_V)  # [TTN][KNT]=[KNT]
+    attention = attention.reshape((dim_K, dim_N * dim_T))  # pack NT back for FC
 
-    # add a fully connected FC layer on top
-    inputs = xp.dot(weights_fc, inputs)
+    # add affine output layer on top
+    attention = xp.dot(weights_o, attention)
 
+    #skip -- TODO: This needs dimensionality to match
+    inputs = attention  # + inputs
+    #layer norm, take [K,NT] inputs, normalize across K channels for each NT
+    inputs = layernorm(inputs)
+    inputs = weights_gamma1 * inputs + weights_beta1
+
+    # FC stack that forms the second part of the block
+    activation = xp.dot(weights_fc1, inputs)
+    activation = relu(activation)
+    activation = xp.dot(weights_fc2, activation)
+
+    # skip -- TODO: This needs dimensionality to match
+    inputs = activation  # + inputs
+
+    #layer norm, take [K,NT] inputs, normalize across K channels for each NT
+    inputs = layernorm(inputs)
+    inputs = weights_gamma2 * inputs + weights_beta2
     return inputs
 
 def loss(params, seq):
@@ -107,8 +147,20 @@ def train_loop():
     weights_k = xp.array(0.01 * np.random.randn(dim_K, dim_C))
     weights_v = xp.array(0.01 * np.random.randn(dim_K, dim_C))
     weights_q = xp.array(0.01 * np.random.randn(dim_K, dim_C))
-    weights_fc = xp.array(0.01 * np.random.randn(data.embed_dim, dim_K))
-    params = [weights_k, weights_v, weights_q, weights_fc]
+    weights_o = xp.array(0.01 * np.random.randn(dim_K, dim_K))
+    # norm
+    weights_gamma1 = xp.array(0.01 * np.random.randn(dim_K, 1))
+    weights_beta1 = xp.array(0.01 * np.random.randn(dim_K, 1))
+    # FC
+    weights_fc1 = xp.array(0.01 * np.random.randn(4*dim_K, dim_K))
+    weights_fc2 = xp.array(0.01 * np.random.randn(data.embed_dim, 4*dim_K))
+    #norm
+    weights_gamma2 = xp.array(0.01 * np.random.randn(data.embed_dim, 1))
+    weights_beta2 = xp.array(0.01 * np.random.randn(data.embed_dim, 1))
+
+    params = [weights_k, weights_v, weights_q, weights_o, weights_gamma1,
+              weights_beta1, weights_fc1, weights_fc2, weights_gamma2,
+              weights_beta2]
 
     losses = []
     sequences = data.seq_iterator(batch_size=dim_N, seq_len=dim_T,
